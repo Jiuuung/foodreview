86be02ff3e9dccc8c25e959702657d1f2f93764a
Jiuuung <kjg682534@kaist.ac.kr>
2022-11-28 19:16:02 +0900

    edit delete restaurant

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.list , name = 'list'),
     path('<int:restaurant_id>/', views.detail , name ='detail'),
     path('addres/', views.add_res, name='restaurant_add'),
+    path('editres/<int:restaurant_id>/', views.edit_res, name ='restaurant_edit'),
+    path('deleteres/<int:restaurant_id>/', views.delete_res, name ='restaurant_delete'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseNotAllowed
 from django.shortcuts import render, get_object_or_404, redirect
 from main.models import Restaurant, Review
 from main.form import RestaurantForm
@@ -24,3 +26,27 @@ def add_res(request):
         form = RestaurantForm
     context={'form':form}
     return render(request, 'main/restaurant_add.html', context)
+
+@login_required(login_url='user:login')
+def edit_res(request, restaurant_id):
+    if request.user.is_superuser:
+        restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
+        if request.method=="POST":
+            form=RestaurantForm(request.POST, instance=restaurant)
+            if form.is_valid():
+                form.save()
+                return redirect('main:detail', restaurant_id)
+        else:
+            form = RestaurantForm(instance=restaurant)
+        context={'form':form}
+        return render(request, 'main/restaurant_add.html',context)
+    else:
+        return HttpResponseNotAllowed('Only Superuser is possible')
+@login_required(login_url='user:login')
+def delete_res(request, restaurant_id):
+    if request.user.is_superuser:
+        restaurant = get_object_or_404(Restaurant, pk=restaurant_id)
+        restaurant.delete()
+        return redirect('main:list')
+    else:
+        return HttpResponseNotAllowed('Only Superuser is possible')
