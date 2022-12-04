20441e380936895f3b5db857bc1343ad2d1c90b7
Ubuntu <ubuntu@ip-172-26-12-70.ap-northeast-2.compute.internal>
2022-12-04 22:12:42 +0000

    change allowed, static, debug

--- config/settings.py
+++ config/settings.py
@@ -27,9 +27,10 @@ environ.Env.read_env(BASE_DIR/'.env')
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.36.250.161']
+STATIC_ROOT = BASE_DIR / 'static/'
 
 
 # Application definition
@@ -123,7 +124,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
 ]
 
 # Default primary key field type
